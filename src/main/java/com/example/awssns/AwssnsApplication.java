39431fd2480de528144cd27458b37715b88ea8f9
d36choi <yuio1126@gmail.com>
2021-03-10 11:24:13 +0900

    Initial commit

---
+++ src/main/java/com/example/awssns/AwssnsApplication.java
@@ -0,0 +1,13 @@
+package com.example.awssns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwssnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwssnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awssns/AwssnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awssns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwssnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
