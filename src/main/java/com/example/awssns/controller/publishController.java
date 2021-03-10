b12121f8d975aa2961f62326c692a489ad10fc85
d36choi <yuio1126@gmail.com>
2021-03-10 15:32:18 +0900

    feat: Add create topic, subscribe feature
    <Change publish http method GET -> POST>
    - json body 로 메시지 내용 지정하게 변경함
    - parameter 로 토픽 지정하게 함
    - 그외에 리팩터링

--- src/main/java/com/example/awssns/controller/publishController.java
+++ src/main/java/com/example/awssns/controller/publishController.java
@@ -3,13 +3,16 @@ package com.example.awssns.controller;
 import com.example.awssns.configuration.AWSConfig;
 import com.example.awssns.service.CredentialService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.regions.Region;
+import org.springframework.web.server.ResponseStatusException;
 import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sns.model.PublishRequest;
-import software.amazon.awssdk.services.sns.model.PublishResponse;
-import java.net.URISyntaxException;
+import software.amazon.awssdk.services.sns.model.*;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -23,13 +26,50 @@ public class publishController {
         this.credentialService = credentialService;
     }
 
-    @GetMapping("/publish")
-    public String publish() throws URISyntaxException {
+    @PostMapping("/createTopic")
+    public ResponseEntity<String> createTopic(@RequestParam final String topicName) {
+        final CreateTopicRequest createTopicRequest = CreateTopicRequest.builder()
+                .name(topicName)
+                .build();
+        SnsClient snsClient = credentialService.getSnsClient();
+        final CreateTopicResponse createTopicResponse = snsClient.createTopic(createTopicRequest);
+
+        if (!createTopicResponse.sdkHttpResponse().isSuccessful()) {
+            throw getResponseStatusException(createTopicResponse);
+        }
+        log.info("topic name = " + createTopicResponse.topicArn());
+        log.info("topic list = " + snsClient.listTopics());
+        snsClient.close();
+        return new ResponseEntity<>("TOPIC CREATING SUCCESS", HttpStatus.OK);
+    }
+
+    @PostMapping("/subscribe")
+    public ResponseEntity<String> subscribe(@RequestParam final String endpoint, @RequestParam final String topicArn) {
+        final SubscribeRequest subscribeRequest = SubscribeRequest.builder()
+                .protocol("https")
+                .topicArn(topicArn)
+                .endpoint(endpoint)
+                .build();
+        SnsClient snsClient = credentialService.getSnsClient();
+        final SubscribeResponse subscribeResponse = snsClient.subscribe(subscribeRequest);
+
+        if (!subscribeResponse.sdkHttpResponse().isSuccessful()) {
+            throw getResponseStatusException(subscribeResponse);
+        }
+        log.info("topicARN to subscribe = " + subscribeResponse.subscriptionArn());
+        log.info("subscription list = " + snsClient.listSubscriptions());
+        snsClient.close();
+        return new ResponseEntity<>("TOPIC SUBSCRIBE SUCCESS", HttpStatus.OK);
+    }
+
+
+    @PostMapping("/publish")
+    public String publish(@RequestParam String topicArn, @RequestBody Map<String, Object> message) {
         SnsClient snsClient = credentialService.getSnsClient();
         final PublishRequest publishRequest = PublishRequest.builder()
-                .topicArn(awsConfig.getSnsTopicARN())
+                .topicArn(topicArn)
                 .subject("HTTP ENDPOINT TEST MESSAGE")
-                .message("11st choi sang hyun test")
+                .message(message.toString())
                 .build();
         PublishResponse publishResponse = snsClient.publish(publishRequest);
         log.info("message status:" + publishResponse.sdkHttpResponse().statusCode());
@@ -39,6 +79,10 @@ public class publishController {
 
     }
 
-
+    private ResponseStatusException getResponseStatusException(SnsResponse response) {
+        return new ResponseStatusException(
+                HttpStatus.INTERNAL_SERVER_ERROR, response.sdkHttpResponse().statusText().get()
+        );
+    }
 
 }
