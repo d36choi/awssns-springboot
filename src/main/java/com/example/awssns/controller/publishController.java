4699027409ae122279f11722b31e7f118d753e88
d36choi <yuio1126@gmail.com>
2021-03-10 13:42:55 +0900

    feat: publish test success
    - creating bean error solved
    - AWSConfig's constructor error solved

---
+++ src/main/java/com/example/awssns/controller/publishController.java
@@ -0,0 +1,51 @@
+package com.example.awssns.controller;
+
+import com.example.awssns.configuration.AWSConfig;
+import com.example.awssns.service.CredentialService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.PublishResponse;
+import java.net.URISyntaxException;
+
+@Slf4j
+@RestController
+public class publishController {
+
+    AWSConfig awsConfig;
+    CredentialService credentialService;
+
+    public publishController(AWSConfig awsConfig, CredentialService credentialService) {
+        this.awsConfig = awsConfig;
+        this.credentialService = credentialService;
+    }
+
+    @GetMapping("/publish")
+    public String publish() throws URISyntaxException {
+        SnsClient snsClient = getSnsClient();
+        final PublishRequest publishRequest = PublishRequest.builder()
+                .topicArn(awsConfig.getSnsTopicARN())
+                .subject("HTTP ENDPOINT TEST MESSAGE")
+                .message("11st choi sang hyun test")
+                .build();
+        PublishResponse publishResponse = snsClient.publish(publishRequest);
+        log.info("message status:" + publishResponse.sdkHttpResponse().statusCode());
+        snsClient.close();
+
+        return "sent MSG ID = " + publishResponse.messageId();
+
+    }
+
+    private SnsClient getSnsClient() {
+        return SnsClient.builder()
+                .credentialsProvider(
+                        credentialService.getAwsCredentials(awsConfig.getAwsAccessKey(), awsConfig.getAwsSecretKey())
+                ).region(Region.AP_NORTHEAST_2)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/example/awssns/service/CredentialService.java
@@ -0,0 +1,15 @@
+package com.example.awssns.service;
+
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+
+@Service
+public class CredentialService {
+
+    public AwsCredentialsProvider getAwsCredentials(String accessKeyID, String secretAccessKey) {
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKeyID, secretAccessKey);
+        return () -> awsBasicCredentials;
+    }
+
+}
