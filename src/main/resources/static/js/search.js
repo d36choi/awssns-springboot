85c225c0351ec781d4ef61987921f0fa54a6221a
d36choi <yuio1126@gmail.com>
2021-03-23 16:43:25 +0900

    feat: Add Search feature in messageList.html
    - search.js 에 검색 로직 생성
    - common.html 헤드만 css import, meta set 이 가능함을 알게됨
    - messageList.html 에 검색위한 input-group 추가

---
+++ src/main/resources/static/js/search.js
@@ -0,0 +1,26 @@
+function search() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("topicSearchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("messageRequestTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+document.querySelector('#topicSearchInput').addEventListener('keyup',function (e) {
+        if (e.key === 'Enter') {
+            search();
+        }
+});
