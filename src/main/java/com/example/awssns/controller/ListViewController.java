1be13e2d85ad62dd2d684ed15f4b4f943e8865cd
d36choi <yuio1126@gmail.com>
2021-03-23 10:48:57 +0900

    style: Add fontawesome icons and change list's th text

--- src/main/java/com/example/awssns/controller/ListViewController.java
+++ src/main/java/com/example/awssns/controller/ListViewController.java
@@ -3,6 +3,7 @@ package com.example.awssns.controller;
 
 import com.example.awssns.entity.MessageRequest;
 import com.example.awssns.service.MessageRequestService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@Slf4j
 @RequestMapping("/list")
 @Controller
 public class ListViewController {
@@ -25,6 +27,9 @@ public class ListViewController {
     public String listTest(Model model) {
         List<MessageRequest> requests = messageRequestService.findAll();
         model.addAttribute("requests",requests);
+        requests.forEach(
+                r -> log.info(r.toString())
+        );
         return "messageList";
     }
 
